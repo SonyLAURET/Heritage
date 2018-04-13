d7b770a7be6194ea64684110f6e467d969567b73
Sony LAURET <sony.lauret@gmail.com>
2018-04-13 16:45:40 +0200

    create a project multithreading and saw edge effect

---
+++ Multithreading/src/main/java/Counter.java
@@ -0,0 +1,16 @@
+
+public class Counter implements Runnable {
+
+	public void run() {
+		int counter=0;
+		
+		while (counter<10) {
+			System.out.print(counter+"");
+			counter++;
+			
+		}
+		
+	}
+
+}
+
